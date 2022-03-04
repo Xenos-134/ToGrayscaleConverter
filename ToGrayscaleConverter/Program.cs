204385fbbb327f1e8ca5c4d5c6e949877fa2dbed
Artem Mordan <87715226+Xenos-134@users.noreply.github.com>
2022-03-04 03:10:17 +0000

    O Modelo Funciona Corretamente

--- ToGrayscaleConverter/Program.cs
+++ ToGrayscaleConverter/Program.cs
@@ -17,7 +17,7 @@ using System.Windows.Forms;
 
 namespace ToGrayscaleConverter
 {
-    internal class Program
+    public class ExtractorProgram
     {
 
         //Devolve dimensao de matriz binaria 
@@ -28,36 +28,28 @@ namespace ToGrayscaleConverter
         }
 
 
-        /*
-            Funcao que faz pre-procesamento de imagem:
-                >Converte tudo para duas cores.
-                >Faz recorte de retangulo onde a imagem esta.
-                >Faz resize para resolucao 500*700
-
-                NOTA: Por enquanto isto nao funciona para imagens de fundo preto devida a pobre resolucao
-                Isso requere calibrar o limiar  a partir da qual sera considerad oque o pixel esta 'Ativado'
-         */
-
-
-
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             int numData = 1000; //numero de amostras de cada dataset
-            if (File.Exists("C:/Users/artem/Desktop/AI/PreProcess/Output.txt"))
+/*            if (File.Exists("C:/Users/artem/Desktop/AI/PreProcess/Output.txt"))
             {
                 File.Delete("C:/Users/artem/Desktop/AI/PreProcess/Output.txt");
             }
-
-            StreamWriter outputFile = new StreamWriter("C:/Users/artem/Desktop/AI/PreProcess/Output.txt");
-
+            StreamWriter outputFile = new StreamWriter("C:/Users/artem/Desktop/AI/PreProcess/Output.txt");*/
 
             //Test with some random params
             string path = "C:/Users/artem/Desktop/AI/PreProcess/trainingSet";
             DirectoryInfo info = new DirectoryInfo(path);
             DirectoryInfo[] directories = info.GetDirectories();
 
+            using (StreamWriter outputFileTest = new StreamWriter("C:/Users/artem/Desktop/AI/PreProcess/testOutput.txt"))
+            {
+                ExtractProperties("C:/Users/artem/Desktop/AI/PreProcess/test.jpg", outputFileTest, "4");
+            }
+
+
             int ic;
-            foreach (var directory in directories)
+/*          foreach (var directory in directories)
             {
                 ic = numData;
                 string label = directory.ToString(); // Label do ficheiro a ser classificado 
@@ -70,11 +62,10 @@ namespace ToGrayscaleConverter
                 foreach (var file in files)
                 {
                     if (ic-- < 0) continue;
-                    //ProcessImage(45, 45, itemsPath+"/"+file.Name, outputFile, label);
                     ExtractProperties(itemsPath + "/" + file.Name, outputFile, label);
                 }
+            }*/
 
-            }
 
 
             Console.WriteLine("Finished Processing all Images");
@@ -85,8 +76,10 @@ namespace ToGrayscaleConverter
 
 
         //Estamos a procesar iamgens de 28 por 28
-        public static void ExtractProperties(string imagePath, StreamWriter file, string label)
+        public static string ExtractProperties(string imagePath, StreamWriter file, string label)
         {
+            string vector = "";
+
             Console.WriteLine($"Label:{label}-Processind image:{imagePath.Split('/').Last()}");
             using (Bitmap image = new Bitmap(imagePath))
             {
@@ -102,19 +95,20 @@ namespace ToGrayscaleConverter
                         if (counter < (image.Width*image.Height - 1))
                         {
                             file.Write($"{pixelValue},");
+                            vector += $"{pixelValue},";
                             counter++;
                         }
                         else
                         { //Last pixel
                             file.Write($"{pixelValue}");
+                            vector += $"{pixelValue}";
                         }
-
-                       // Console.Write($"[{pixelValue}] ");
                     }
-                    //Console.WriteLine();
+              
                 }
             }
             file.Write(Environment.NewLine);
+            return vector;
         }
 
     }
